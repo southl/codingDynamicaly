ede380ccdf7fbd49dc41f4697ae71ebe58c3e95e
southl <gyrii84@gmail.com>
2017-09-26 12:58:27 +0300

    change menu.php

--- menu.php
+++ menu.php
@@ -3,6 +3,17 @@
 	include('includes/header.php');
 ?>
 
+<div id="menu-items"></div>
+<h1>Our Deliicos menu</h1>
+<p>some text about  menu</p>
+<p><em>LOOK HERE</em></p>
+
+<hr>
+<ul>
+		
+</ul>
+
+
 <?php 
 	include('includes/footer.php');
 ?>
