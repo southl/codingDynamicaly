e4adf5c7e5c094082688b2e62c5a29f913021d5a
southl <gyrii84@gmail.com>
2017-10-05 17:01:40 +0300

    fixed error undefined var message in line 93

--- contact.php
+++ contact.php
@@ -2,6 +2,7 @@
 	define("TITLE", "Contact Us | Franklin's Fine Dining");
 	
 	include('includes/header.php');
+	$message ='null';
 	
 	/*
 		NOTE:
@@ -84,7 +85,7 @@
 			}
 			
 			// Add the recipient email to a variable
-			$to	= "your@email.com";
+			$to	= "brad@brightsidestudios.ca";
 			
 			// Create a subject
 			$subject = "$name sent a message via your contact form";
@@ -101,27 +102,31 @@
 				$message .= "\r\n\r\nPlease add $email to the mailing list.\r\n";
 				
 			}
-
-			$message = wordwrap($message, 72);
-
+		
+			$message = wordwrap($message, 72); // Keep the message neat n' tidy
+		
+			// Set the mail headers into a variable
 			$headers = "MIME-Version: 1.0\r\n";
-			$headers .= "Content-type: text/plain; charset=iso-8826-1\r\n";
-			$headers .= "From: $name <$email>\r\n";
+			$headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
+			$headers .= "From: " . $name . " <" . $email . ">\r\n";
 			$headers .= "X-Priority: 1\r\n";
-			$headers .= "X-MSMail-Priority: Hight\r\n\r\n";
-
-			//send the email
-
+			$headers .= "X-MSMail-Priority: High\r\n\r\n";
+		
+			
+			// Send the email!
 			mail($to, $subject, $message, $headers);
+		?>
+		
+		<!-- Show success message after email has sent -->
+		<h5>Thanks for contacting Franklin's!</h5>
+		<p>Please allow 24 hours for a response.</p>
+		<p><a href="/final" class="button block">&laquo; Go to Home Page</a></p>
+		
+		<?php
+			} else {
+		?>
 
-
-	?>
-<!--show succsses message-->
-<h5>THanks for contact</h5>
-<p> Please allow 24 for responsing</p>
-<p><a href="/final" class="button block">&laquo; Go to Home Page</a></p>
-	<?php } else { ?>
-	<form method="post" action="" id="contact-form">
+		<form method="post" action="" id="contact-form">
 			<label for="name">Your name</label>
 			<input type="text" id="name" name="name">
 			
@@ -135,11 +140,11 @@
 			
 			<input type="submit" class="button next" name="contact_submit" value="Send Message">
 		</form>
-
-		<?php } ?>
 		
+		<?php
+			}
+		?>
 		<hr>
-
 	</div><!-- contact -->
 			
 <?php include('includes/footer.php'); ?>
